a331873a72d35ea87c493cfa555045bcd540ed60
Shanee Cuevas <cuevasshanesha@gmail.com>
2022-12-15 00:52:10 +0800

    [Ngalot] Deploy to Heroku

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q)u+7$i_yln3g(9c9kiuqe4g-gys^cy@lfzw!e%1xe+=z6$rny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['historyplotter.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

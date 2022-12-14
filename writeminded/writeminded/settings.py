59097be5d72297b64d1e122bd2258caee90b1e87
Shanee Cuevas <cuevasshanesha@gmail.com>
2022-12-15 01:18:47 +0800

    try again

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -130,13 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

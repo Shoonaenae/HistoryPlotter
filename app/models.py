acf2a957d83f4055b5a6df0c61ce024628579a4f
John.N <ngalot3@gmail.com>
2022-12-15 10:15:10 +0800

    risky move. but files writeminded folder moved upward

---
+++ app/models.py
@@ -0,0 +1,98 @@
+from ckeditor.fields import RichTextField
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+import os
+import datetime
+
+# Create your models here.
+def filepath(request, filename):
+    old_filename = filename
+    timeNow = datetime.datetime.now().old_filename('%Y%m%d%H:%M:%S')
+    filename = "%s%s" % (timeNow, old_filename)
+
+    return os.path.join('uploads/', filename)
+
+class Post(models.Model):
+    body = RichTextField(blank=True, null = True)
+
+class User(models.Model): 
+    #user id i think automatic na ma generate .. try
+    username = models.CharField(max_length = 20, unique=True, null=False) #added unique 
+    password = models.CharField(max_length = 50)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email_address = models.CharField(max_length = 50, unique = True , null = False)
+
+# Project
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class uploadfilemodel(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=20, unique=True, null=True)
+    description = models.CharField(max_length=50, null=True)
+    file = models.FileField(null = True)
+    cover = models.ImageField(null = True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "uploadfile"
+
+class groupmodel(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    ideafile = models.CharField(max_length=200, unique=True, null=True)
+    name = models.CharField(max_length=20, unique=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "groupfiles"
+
+# quiz
+
+
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    quiz_name = models.CharField(max_length=100)
+    quiz_desc = models.TextField(default=None)
+    quiz_date = models.DateField(blank=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    #quiz_type = models.CharField(max_length=50)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete= models.CASCADE)
+    q_num = models.IntegerField()
+    question = models.CharField(max_length=500)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete= models.CASCADE)
+    answer = models.CharField(max_length= 100)
+    isAnswer = models.BooleanField(default=False)
+
+# end quiz
+
+class Materials(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    period = models.CharField(max_length=100, null=True)
+    description = models.TextField()
+    actual_material = RichTextField(blank=True, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+    
+    def get_absolute_url(self):
+        return reverse('material-details', args=(str(self.id)))
+
+#Relations
+class Relations(models.Model):
+    name = models.CharField(max_length=100)
+    ideafile = models.ForeignKey(uploadfilemodel, on_delete=models.CASCADE)
+    materials = models.ForeignKey(Materials, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
---
+++ app/urls.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from cgitb import html
+from typing import ValuesView
+from django.urls import path
+from . import views
+from .views import MaterialsView, MaterialDetailView, AddMaterialsView, UpdateMaterialDescriptionsView, DeleteMaterialDescriptionView
+
+urlpatterns = [
+    path('ungroup/<str:pk>', views.ungroup, name = "ungroup"),
+    path('editgroup/<str:pk>', views.editgroup.as_view(), name = "editgroup"),
+    path('group', views.groupfiles.as_view(), name = "group"),
+    path('ideanestdelete/<str:pk>', views.ideanestdelete, name = "ideanestdelete"),
+    path('ideaNestEdit/<int:pk>', views.ideaNestEdit.as_view(), name = "ideaNestEdit"),
+    path('ideaNest', views.ideaNest.as_view(), name = "ideaNest"),
+
+    path('', views.LandingPageView.as_view(), name = 'LandingPage'),
+    path('About', views.AboutView.as_view(), name = 'About'),
+
+    path('Signin', views.SigninView.as_view(), name = 'Signin'),
+
+    path('Signup', views.SignupView.as_view(), name = 'Signup'),
+
+
+    path('ProjectDashboard', views.ProjectDashboard.as_view(), name = 'ProjectDashboard'),
+    path('ProjectView ', views.ProjectView.as_view(), name = "ProjectView"),
+    path('Relations', views.Relation.as_view(), name = "Relations"),
+
+    path('CreateQuiz', views.CreateQuizView.as_view(), name = "CreateQuiz"),
+    path('CreateQuiz/EditQuiz/', views.EditQuizView.as_view(), name = "EditQuiz"),
+
+    #path('EditQuizOptions/<int:question_id>', views.EditQuizOptions, name = "EditQuizOptions"),
+    path('EditQuizOptions/<int:question_id>', views.EditQuizOptionsView.as_view(), name = "EditQuizOptions"),
+    #path('CreateQuiz/EditQuiz/EditQuizOptions/(?<question_id>\d+)/$', views.EditQuizOptionsView.as_view(), name = "EditQuizOptions"),
+
+    path('CreateQuiz/EditQuiz/EditQuizOptions/Quiz', views.QuizView.as_view(), name = "Quiz"),
+
+    path('write/', MaterialsView.as_view(), name="materials"),
+    path('materials/<int:pk>', MaterialDetailView.as_view(), name="material-details"),
+    #pk->primary key... unique id number assigned to new database record entry
+    path('add_material/', AddMaterialsView.as_view(), name = 'add-materials'),
+    path('material/edit/<int:pk>', UpdateMaterialDescriptionsView.as_view(), name = 'update-material'),
+    path('material/<int:pk>/delete', DeleteMaterialDescriptionView.as_view(), name = 'delete-material'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

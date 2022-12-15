bbed2e83fae68d7f41c2a39e89622e52864017cb
John.N <ngalot3@gmail.com>
2022-12-15 10:23:51 +0800

    static

--- writeminded/settings.py
+++ writeminded/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')

0675f13ee696726bf979bd9fdc3cd1b5f504c050
John.N <ngalot3@gmail.com>
2022-12-15 08:09:46 +0800

    To be able to run locally

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

3edb1e999d93b1caa2b2a21bac0d1685e16d97c2
John.N <ngalot3@gmail.com>
2022-12-19 22:58:13 +0800

    Modify settings.py
    Fix error Django Forbidden (403) CSRF verification failed. Request aborted.

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -141,3 +141,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['historyplotter-production.up.railway.app']

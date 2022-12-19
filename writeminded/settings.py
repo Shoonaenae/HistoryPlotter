05c2844a1176b53f9bc5666ee46760eba4201dca
John.N <ngalot3@gmail.com>
2022-12-19 18:10:07 +0800

    heroku settings

--- writeminded/settings.py
+++ writeminded/settings.py
@@ -139,13 +139,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 
 # Enable WhiteNoise's GZip compression of static assets.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

33cbd4d46e9aa2da749ceeea1a190408ede56be5
John.N <ngalot3@gmail.com>
2022-12-20 15:11:02 +0800

    modify settings.py, runtime, requirements
    updated

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

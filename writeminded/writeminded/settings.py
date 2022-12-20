c71e15547aaedf50b745a070e5100de75033a8bb
John.N <ngalot3@gmail.com>
2022-12-20 23:59:49 +0800

    Update settings.py
    dj-database-url

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

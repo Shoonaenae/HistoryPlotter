9bcb294db710106b5069a5e43e365f88ebef69f0
John.N <ngalot3@gmail.com>
2022-12-21 00:15:39 +0800

    update admin.py, urls.py
    django admin db

--- writeminded/app/admin.py
+++ writeminded/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(uploadfilemodel)
+admin.site.register(groupmodel)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Materials)
+admin.site.register(Relations)
--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -76,24 +76,23 @@ WSGI_APPLICATION = 'writeminded.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'historyplotter',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'historyplotter',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
 
 
 # Password validation

7ff28a88d7ee99ce4b46399df9f0b4ca3700e8ac
John.N <ngalot3@gmail.com>
2022-12-20 15:13:37 +0800

    Revert "modify settings.py, runtime, requirements"
    This reverts commit 33cbd4d46e9aa2da749ceeea1a190408ede56be5.

--- writeminded/writeminded/settings.py
+++ writeminded/writeminded/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'app/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
